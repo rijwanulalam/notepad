7467758e81f5dc13e4fed7c541709d27e550a057
rijwanulalam <rijwanul007@gmail.com>
2023-11-14 15:52:25 +0600

    color fixed

--- js/script.js
+++ js/script.js
@@ -7,24 +7,24 @@ function handleFormat(textId) {
   return text;
 }
 
-var colors;
-var defaultColor = "#0000";
+let colors;
+let defaultColor = "#0000";
 window.addEventListener("load", startup, false);
 function startup() {
   colors = document.querySelector("#colors");
-  colors.value = defaultColor;
+//   colors.value = defaultColor;
   colors.addEventListener("input", updateFirst, false);
-  colors.addEventListener("change", false);
+  colors.addEventListener("change", updateAll, false);
   colors.select();
 }
 function updateFirst(event) {
-//   var p = text;
-  if (text) {
-    text.style.color = event.target.value;
+  var p = text;
+  if (p) {
+    p.style.color = event.target.value;
   }
 }
-// function updateAll(event) {
-//   document.querySelectorAll("p").forEach(function (text) {
-//     p.style.color = event.target.value;
-//   });
-// }
+function updateAll(event) {
+  document.querySelectorAll("p").forEach(function (text) {
+    p.style.color = event.target.value;
+  });
+}
