51946f7584b61de42f15a9d47d008412a42996d4
rijwanulalam <rijwanul007@gmail.com>
2023-11-14 17:09:46 +0600

    toggle button added

--- js/font-style.js
+++ js/font-style.js
@@ -1,15 +1,31 @@
 // font style button
 document.getElementById("font-bold").addEventListener("click", function(){
     handleFormat("text");
-    text.style.fontWeight = "bold";
+    if(text.style.fontWeight === "bold"){
+        text.style.fontWeight = "normal";
+    }
+    else{
+        text.style.fontWeight = "bold";
+    }
 })
 
 document.getElementById("font-italic").addEventListener("click", function(){
     handleFormat("text");
-    text.style.fontStyle = "italic";
+
+    if(text.style.fontStyle === "italic"){
+        text.style.fontStyle = "normal";
+    }
+    else{
+        text.style.fontStyle = "italic";
+    }
 })
 
 document.getElementById("font-underline").addEventListener("click", function(){
     handleFormat("text");
-    text.style.textDecoration = "underline";
+    if(text.style.textDecoration === "underline"){
+        text.style.textDecoration = "none";
+    }
+    else{
+        text.style.textDecoration = "underline";
+    }
 })
